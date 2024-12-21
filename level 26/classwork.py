45f0153ba437453832e3af0854bf60a8d252d0c8
dachi-pixel <dachi.bolqvadze11@gmail.com>
2024-12-21 18:32:07 +0400

    first commit

---
+++ level 26/classwork.py
@@ -0,0 +1,30 @@
+#1
+li =[1, 2, 3, 4]
+print(li[1:3])
+
+#2
+
+lis =[1, 2, 3, 4, 5]
+print(lis[0:])
+
+#3
+
+list=[1, "hi", 1 > 10, 3.2]
+print(list[-5:])
+
+#4
+
+listt = [1, 2, 3, 4, 5]
+listt[3] = listt[4]
+listt[4] = 4
+print(listt)
+
+#5
+
+name = "goalorientedacademy"
+print(name[0:4])
+
+#6
+
+listtt=[1, 2, 3, 4, 5]
+print(listtt[::-1])
