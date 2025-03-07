c40bb2fe6467e6e69d9b27d2d0420346378c0ca9
dachi-pixel <dachi.bolqvadze11@gmail.com>
2025-03-07 18:27:21 +0400

    commit

---
+++ level 38/homework/python.py
@@ -0,0 +1,54 @@
+city = "tbillisi" .upper()
+print (city)
+
+#2)
+
+print(input("enter any word: ").upper())
+
+#3)
+
+print("hello world" .upper())
+
+#4)
+
+name = "GABRIEL" .lower()
+
+print(name)
+
+#5)
+
+print(input("enter any word: ").lower())
+
+#6)
+
+text = "PYTHON IS FUN" .lower()
+
+print(text)
+
+#7)
+
+name = "davit" .capitalize()
+
+print(name)
+
+#8)
+
+input("enter any word: ").capitalize()
+
+#9)
+
+print("hello world" .capitalize())
+
+#10)
+
+text = "programming" .find("g")
+
+print(text)
+
+#11)
+
+print("hello world" .find("world"))
+
+#12)
+
+print("python is fun" .find("i"))
